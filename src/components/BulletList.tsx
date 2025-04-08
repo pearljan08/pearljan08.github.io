af0111cca2ef3b4cf080c23361beba83b975a3ec
Pearl Jan Sabanal <pearljan08@gmail.com>
2025-04-08 13:25:04 +0800

    added other experience

--- src/components/BulletList.tsx
+++ src/components/BulletList.tsx
@@ -21,31 +21,32 @@ interface BulletListProps {
 
 export function BulletList({ list }: BulletListProps) {
   return (
-    
-      <Timeline>
-        {
-            list.map((item, index) => (
-                <TimelineItem key={index}>
-                <TimelineConnector />
-                <TimelineHeader className="h-3">
-                    <TimelineIcon />
-                    <p className="text-gray-700 leading-none font-bold">
-                    {item.title} { item.client ? `(${item.client})` : ""}
-                    </p>
-                </TimelineHeader>
-                <TimelineBody className="pb-8">
-                    <p className="text-gray-600 font-semibold">{item.company}</p>
-                    <p className="text-sm text-gray-600">{item.date}</p>
-                    {/* {
-                        item.description.map((desc, index) => (
-                            <p key={index}>{desc}</p>
-                        ))
-                    } */}
-                </TimelineBody>
-                </TimelineItem>
-            ))
-        }
-      </Timeline>
+      <div className="">
+        <Timeline>
+          {
+              list.map((item, index) => (
+                  <TimelineItem key={index}>
+                  <TimelineConnector />
+                  <TimelineHeader className="h-1">
+                      <TimelineIcon />
+                      <p className="text-gray-700 leading-none font-bold">
+                      {item.title} { item.client ? `(${item.client})` : ""}
+                      </p>
+                  </TimelineHeader>
+                  <TimelineBody className="pb-8">
+                      <p className="text-gray-600 font-semibold">{item.company}</p>
+                      <p className="text-sm text-gray-600">{item.date}</p>
+                      {/* {
+                          item.description.map((desc, index) => (
+                              <p key={index}>{desc}</p>
+                          ))
+                      } */}
+                  </TimelineBody>
+                  </TimelineItem>
+              ))
+          }
+        </Timeline>
+      </div>
     
   );
 }
--- src/components/ExperienceList.tsx
+++ src/components/ExperienceList.tsx
@@ -36,13 +36,35 @@ export const experienceList = [
     },
     {
         title: "Web Developer",
+        client: "GCG - Project Based",
+        company: "Autumn",
+        date: "November 2020 - December 2020",
+        description: [
+            "Create modules required for completion of the project",
+            "Used CodeIgniter",
+        ],
+        logo: pinnacleLogo
+    },
+    {
+        title: "Web Developer",
         client: "",
         company: "Pinnacle Technologies",
-        date: "January 2022 - August 2024",
+        date: "September 2019 - August 2020",
         description: [
             "Create and test apps/modules for the website",
             "Quality-test and troubleshoot the website and/or its applications in various browsers",
             "Create websites and user interfaces, using standard HTML/CSS practices, while incorporating data from back-end databases and services"
+        ],
+        logo: pinnacleLogo
+    },
+    {
+        title: "Graphic Designer",
+        client: "",
+        company: "Lighthouse Enterprise",
+        date: "May 2019 - September 2019",
+        description: [
+            "Design and create graphics for their eCommerce platform",
+            "Design and create graphics for their social media accounts"
         ],
         logo: pinnacleLogo
     }
