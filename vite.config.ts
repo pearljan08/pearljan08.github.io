46934375c16dfe3faf294d977e5de4d49e21c4ac
Pearl Jan Sabanal <pearljan08@gmail.com>
2025-04-08 13:38:27 +0800

    added changes for build to run

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  base: '/pearljan08.github.io/',
+  base: '/',
 })
